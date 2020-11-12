916094ffdd921087ff57a8d34c0d7b939088a909
Alexandra Dima <alexandra.dima89@yahoo.com>
2020-11-12 11:59:41 +0100

    Updates

---
+++ precache-manifest.19ec161c923b655d9f091d9bb8a52001.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7536252e7d28677dd29d73eb3a3e50e5",
+    "url": "/drinks-order/index.html"
+  },
+  {
+    "revision": "084c4a71d31b24303bba",
+    "url": "/drinks-order/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dab9ca4a59d9b3117707",
+    "url": "/drinks-order/static/css/main.81a0be5f.chunk.css"
+  },
+  {
+    "revision": "084c4a71d31b24303bba",
+    "url": "/drinks-order/static/js/2.af46ab71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drinks-order/static/js/2.af46ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab9ca4a59d9b3117707",
+    "url": "/drinks-order/static/js/main.ee2e4e59.chunk.js"
+  },
+  {
+    "revision": "8c2f27064d94d0d07edb",
+    "url": "/drinks-order/static/js/runtime-main.a406cc0c.js"
+  },
+  {
+    "revision": "f8385f6713bb518915037d3f66d7e703",
+    "url": "/drinks-order/static/media/logo.f8385f67.svg"
+  }
+]);
